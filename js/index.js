6e8cefd0a73a6cc9fc7fe2fb0c9fc07ad6a68fe9
nicod12 <dumenicolas45@gmail.com>
2024-07-13 16:23:22 -0300

    boton menu actualizado

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
-//abre el manu hamburguesa
-function cambiarClaseMenu(){
-    let siteNav = document.getElementById("site__nav");
-        siteNav.classList.toggle("site__navAbrir");
-        // hace el efecto de hamurguesa a cruz
-    let menuAbierto = document.getElementById("menu-toggle");
-        menuAbierto.classList.toggle("menu-abierto");
-    }
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btnToggle = document.getElementById("menu-toggle");
+
+    //abre el manu hamburguesa
+    function cambiarClaseMenu(){
+        let siteNav = document.getElementById("site__nav");
+            siteNav.classList.toggle("site__navAbrir");
+            // hace el efecto de hamurguesa a cruz
+        let menuAbierto = document.getElementById("menu-toggle");
+            menuAbierto.classList.toggle("menu-abierto");
+    };
+    
+    btnToggle.addEventListener('click', () => {
+        cambiarClaseMenu();
+    });
+
+})
+
 
