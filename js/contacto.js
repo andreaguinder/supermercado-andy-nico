a1146e296dbafce91ca161e971d87660dc7757a5
nicod12 <dumenicolas45@gmail.com>
2024-07-13 18:07:25 -0300

    actualizacion de queries

--- js/contacto.js
+++ js/contacto.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("form");
+
   const nombreValue = document.getElementById("nombre");
   const apellidoValue = document.getElementById("apellido");
   const emailValue = document.getElementById("email");
   const mensajeValue = document.getElementById("mensaje");
 
+  const boxNombre = document.getElementById("box1");
+  const boxApellido = document.getElementById("box2");
+  const boxEmail = document.getElementById("box3");
+  const boxMensaje = document.getElementById("box4");
+
   const validateForm = () => {
+    const errorText = document.createElement("span");
     if (nombreValue.value.trim() === "") {
       console.log("Por favor ingrese nombre!");
       return;
@@ -25,11 +32,6 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("Por favor ingrese su mensaje!");
       return;
     }
-
-    console.log(nombreValue.value);
-    console.log(apellidoValue.value);
-    console.log(emailValue.value);
-    console.log(mensajeValue.value);
 
     alert("Mensaje enviado exitosamente!");
   };
