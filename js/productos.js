1ce82f9a5ad56de402f65b4140cead0af7500407
nicod12 <dumenicolas45@gmail.com>
2024-07-16 20:47:35 -0300

    se agrego  modal no hay stock disponible

--- js/productos.js
+++ js/productos.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "1350",
   ];
 
-  const sctockProducts = [10, 15, 11, 25, 33, 5, 12, 14, 17];
+  const stockProducts = [10, 15, 11, 25, 33, 5, 12, 14, 17];
 
   const imagesProducts = [
     "../images/fideos-marolio.webp",
@@ -36,6 +36,35 @@ document.addEventListener("DOMContentLoaded", () => {
     "../images/cubo-rubik.jpg",
     "../images/coca-cola.jpg",
   ];
+
+  const renderModal = () => {
+    const containerModal = document.createElement("div");
+    containerModal.classList.add("modalContainerExito");
+
+    const modalForm = document.createElement("div");
+    modalForm.classList.add("modalForm");
+
+    const buttonModal = document.createElement("button");
+    buttonModal.classList.add("btnModal", "pt-2", "px-3");
+    buttonModal.id = "cerrarModal";
+
+    buttonModal.addEventListener("click", () => {
+      containerModal.remove();
+    });
+
+    const icon = document.createElement("i");
+    icon.classList.add("fa-solid", "fa-x");
+
+    const message = document.createElement("h5");
+    message.textContent = "No hay más stock disponible.";
+
+    buttonModal.appendChild(icon);
+    modalForm.appendChild(buttonModal);
+    modalForm.appendChild(message);
+    containerModal.appendChild(modalForm);
+
+    document.body.appendChild(containerModal);
+  };
 
   const renderProducts = () => {
     nameProducts.forEach((nameProduct, index) => {
@@ -57,7 +86,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const labelStockProduct = document.createElement("h6");
       labelStockProduct.classList.add("card-title", "grande");
-      labelStockProduct.textContent = `Stock ${sctockProducts[index]} unidades`;
+      labelStockProduct.id = `stock-${index}`;
+      labelStockProduct.textContent = `Stock ${stockProducts[index]} unidades`;
 
       const labelPriceProduct = document.createElement("h6");
       labelPriceProduct.textContent = `$${priceProducts[index]} por unidad`;
@@ -68,13 +98,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const btnAdd = document.createElement("button");
       btnAdd.type = "button";
-      btnAdd.id = "btnAdd";
+      btnAdd.id = `btnAdd-${index}`;
       btnAdd.classList.add("btnModal", "px-3", "mb-0");
       btnAdd.textContent = "+";
 
       const input = document.createElement("input");
       input.type = "text";
       input.placeholder = 0;
+      input.id = `input-${index}`;
 
       span.appendChild(btnAdd);
       span.appendChild(input);
@@ -87,9 +118,16 @@ document.addEventListener("DOMContentLoaded", () => {
       containerCard.appendChild(cardBody);
 
       containerProducts.appendChild(containerCard);
+
       btnAdd.addEventListener("click", () => {
         let currentValue = parseInt(input.value) || 0;
-        input.value = currentValue + 1;
+        if (stockProducts[index] > 0) {
+          input.value = currentValue + 1;
+          stockProducts[index]--;
+          labelStockProduct.textContent = `Stock ${stockProducts[index]} unidades`;
+        } else {
+          renderModal();
+        }
       });
     });
   };
