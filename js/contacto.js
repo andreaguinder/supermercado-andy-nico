dab3dbf8010f92e67111786f1d64cd2475ef35d5
nicod12 <dumenicolas45@gmail.com>
2024-07-16 11:26:11 -0300

    mejora funciones

--- js/contacto.js
+++ js/contacto.js
@@ -1,88 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("form");
 
-  const nombreValue = document.getElementById("nombre");
-  const apellidoValue = document.getElementById("apellido");
+  const nameValue = document.getElementById("nombre");
+  const surnameValue = document.getElementById("apellido");
   const emailValue = document.getElementById("email");
-  const mensajeValue = document.getElementById("mensaje");
-
-
-  const boxNombre = document.getElementById("box1");
-  const boxApellido = document.getElementById("box2");
-  const boxEmail = document.getElementById("box3");
-  const boxMensaje = document.getElementById("box4");
+  const phoneValue = document.getElementById("phone");
+  const messageValue = document.getElementById("mensaje");
 
   const validateForm = () => {
-    const errorText = document.createElement("span");
-    errorText.classList.add("errorMensaje");
-    if (nombreValue.value.trim() === "") {
-      errorText.textContent = "Por favor ingrese nombre";
-      boxNombre.appendChild(errorText);
+    if (nameValue.value.trim() === "") {
+      const errorMessage = document.createElement("span");
+      errorMessage.classList.add("errorMensaje");
+      errorMessage.textContent = "Todos los campos son obligatorios";
+
+      form.appendChild(errorMessage);
+
       setTimeout(() => {
-        errorText.remove();
+        errorMessage.remove();
       }, 1500);
 
       return;
     }
-
-    if (apellidoValue.value.trim() === "") {
-      errorText.textContent = "Por favor ingrese Apellido";
-      boxApellido.appendChild(errorText);
-      setTimeout(() => {
-        errorText.remove();
-      }, 1500);
-      return;
-    }
-
-    if (emailValue.value.trim() === "") {
-      errorText.textContent = "Por favor ingrese Email valido";
-      boxEmail.appendChild(errorText);
-      setTimeout(() => {
-        errorText.remove();
-      }, 1500);
-      return;
-    }
-
-    if (mensajeValue.value.trim() === "") {
-      errorText.textContent = "Por favor ingrese un mensaje";
-      boxMensaje.appendChild(errorText);
-      setTimeout(() => {
-        errorText.remove();
-      }, 1500);
-      return;
-    }
-
     renderModal();
-
   };
 
   const renderModal = () => {
-
     const containerModal = document.createElement("div");
     containerModal.classList.add("modalContainerExito");
-
 
     const modalForm = document.createElement("div");
     modalForm.classList.add("modalForm");
 
-
     const buttonModal = document.createElement("button");
     buttonModal.classList.add("btnModal", "pt-2", "px-3");
     buttonModal.id = "cerrarModal";
-
 
     buttonModal.addEventListener("click", () => {
       containerModal.remove();
       resetForm();
     });
 
-
     const icon = document.createElement("i");
     icon.classList.add("fa-solid", "fa-x");
 
     const success = document.createElement("h5");
     success.textContent = "¡Su consulta fue enviada!";
-
 
     modalForm.appendChild(buttonModal);
     modalForm.appendChild(success);
@@ -103,7 +65,5 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
 
     validateForm();
-
-
   });
 });
